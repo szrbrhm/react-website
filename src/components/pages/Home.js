96004f8d1ed00a476db484da529e886e20caa639
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-17 22:21:02 +0200

    create Home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import '../../App.css';
+import HeroSection from '../HeroSection';
+
+
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+        <HeroSection/>
+            
+        </>
+    )
+}
+
+export default Home
